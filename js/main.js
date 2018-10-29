9f2c5c21d4392a99c7c4a4aa3c655fa291fe0271
costagavras <cgavras@yahoo.ca>
2018-10-29 15:10:14 -0400

    switch OK

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 
+var btn = document.querySelector('.switch, on');
+var body = document.querySelector('body');
+var txt = document.querySelector('h1, .status');
+var light_on;
 
+
+btn.addEventListener("click", function(){
+  (btn.className) === "switch on" ? light_on = true : light_on = false;
+  if (light_on) {
+    btn.className = "switch off";
+    body.className = "dark";
+    // txt.className = "light";
+
+    txt.innerText = "Hey, who turned off the lights?";
+  } else {
+    btn.className = "switch on";
+    body.className = "light";
+    // txt.className = "dark";
+    txt.innerText = "It's so bright in here!";
+  }
+});
